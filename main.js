0c31098af0794df49a71a2261f1c975fddd5e653
jodelajo <jodelajo@gmail.com>
2021-02-17 12:33:13 +0100

    opdracht 5

--- main.js
+++ main.js
@@ -241,25 +241,84 @@ sold.textContent = soldTvs
 //opdracht 4: Geef de type-namen van twee tv's weer op de pagina. Welke tv's dat precies zijn, maakt niet zoveel uit.
 //Voor nu betekent dit dat je het appenden van de nodes twee keer moet uitschrijven, dat is niet erg!
 
-const tv1 = inventory.find((tv) =>{
-  return tv.type === '55PUS7805';
-})
-console.log(tv1)
+function tvTypes(tvType) {
+  return `${tvType.type}`;
+}
+const typeTV = tvTypes(inventory[3]);
+console.log(typeTV)
 
-const tv2 = inventory.find((tv) =>{
-  return tv.type === '43HAK6152';
-})
-console.log(tv2)
+//Opdracht 5a: Zorg ervoor dat er een string wordt gegenereerd voor de naam van een tv.
+// Maak een functie die één tv-object als parameter verwacht en de naam op de volgende manier samenvoegt:
+// [merk] [type] - [naam] zoals Philips 43PUS6504/12 - 4K TV of NIKKEI NH3216SMART - HD smart TV.
+// Zorg ervoor dat je deze functie voor iedere tv zou kunnen gebruiken.
+
+function tvNames(tvName) {
+  return `${tvName.brand} ${tvName.type} - ${tvName.name}`
+}
+const nameTv = tvNames(inventory[3]);
+console.log(nameTv)
+
+//Opdracht 5b: Zorg ervoor dat de prijs van een tv netjes geformat wordt.
+//Maak een functie die één tv-prijs als parameter verwacht (zoals 379) en daar
+//de volgende string van maakt: €379,-. Zorg ervoor dat je deze functie voor iedere tv zou kunnen gebruiken.
+
+function tvPrices(tvPrice) {
+  return `€ ${tvPrice.price},-`
+}
+const priceTv = tvPrices(inventory[3])
+console.log(priceTv)
+
+//Opdracht 5c: Zorg ervoor dat er een string wordt gegenereerd voor alle beschikbare schermgroottes van één tv in zowel
+//inches als cm Maak een functie die één screen-sizes array verwacht en de groottes op de volgende manier samenvoegt:
+//[schermgrootte] inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm) etc.
+//Dus een input van [32] geeft 32 inch (81 cm)
+//en een input van [43, 50, 55, 58] geeft 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm).
+//Zorg ervoor dat je deze functie voor iedere tv zou kunnen gebruiken, zowel voor tv's met maar één schermgrootte als met tientallen schermgroottes.
+
+function screenSizesTV(screenSize) {
+// console.log("ScreenSizes?", screenSize.availableSizes);
+  let allSizes = "";
+  for (let i = 0; i < screenSize.availableSizes.length; i++) {
+// console.log("1 ScreenSize:", screenSize.availableSizes[i]); // 1 voor 1 gelogd
+    const currentScreenSize = screenSize.availableSizes[i]
+// console.log(currentScreenSize);
+// const formattedSizeWithPike = `${currentScreenSize} inches (${Math.round(currentScreenSize * 2.54)} cm) |`;
+// console.log(formattedSizeWithPike);
+//     allSizes = allSizes + formattedSizeWithPike;
+//     console.log(allSizes);
+//     const lastEntrySize = screenSize.availableSizes.length -1;
+//     console.log("Gelijk aan:" , lastEntrySize, screenSize.availableSizes.length -1 === i);
+
+    if (screenSize.availableSizes.length - 1 === i) {
+      // console.log("Last:" )
+      const formattedSize = `${currentScreenSize} inches (${Math.round(currentScreenSize * 2.54)} cm)`;
+      // console.log(formattedSize);
+      allSizes = allSizes + formattedSize;
+
+    } else {
+      // console.log("Pike toevoegen?");
+      const formattedSizeWithPike = `${currentScreenSize} inches (${Math.round(currentScreenSize * 2.54)} cm) | `;
+      // console.log(formattedSizeWithPike);
+      allSizes = allSizes + formattedSizeWithPike;
+    }
+  }
+  return allSizes;
+}
+const sizeTV = screenSizesTV(inventory[3])
+console.log(sizeTV)
+
+//Opdracht 5d: Zorg ervoor de informatie van één van de twee tv's zoals het voorbeeld wordt weergegeven op de pagina.
+//Gebruik hiervoor de functies die je hebt gemaakt in opdracht 5a, 5b en 5c.
+
+// Philips 43PUS6504/12 - 4K TV
+// €379,-
+//     43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm)
 
 //DOM BIJ OPDRACHT 4
 const listOfTv = document.getElementById('list');
 const firstTv = document.createElement('li');
 firstTv.setAttribute('class', 'tv-item');
-firstTv.textContent = tv1.type;
-const secTv = document.createElement('li');
-secTv.setAttribute('class', 'tv-item');
-secTv.textContent = tv2.type;
+firstTv.setAttribute('style', 'white-space:pre');
+firstTv.textContent = `${nameTv} \n ${priceTv}  \n ${sizeTV}`;
 listOfTv.appendChild(firstTv)
-listOfTv.appendChild(secTv)
-
 
